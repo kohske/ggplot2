133bbab875d651068d3a98a9741894ebbee2703c
Kohske Takahashi @ ayaka <takahashi.kohske@gmail.com>
2012-03-05 17:24:45 +0900

    facet-null: code improvement. now all facets use same flow.

--- R/facet-null.r
+++ R/facet-null.r
@@ -38,52 +38,44 @@ facet_render.null <- function(facet, panel, coord, theme, geom_grobs) {
     respect <- TRUE
   }
   
-  fg <- coord_render_fg(coord, range, theme)
-  bg <- coord_render_bg(coord, range, theme)
+  table <- gtable(widths = rep(1, 3), heights = rep(1, 3), respect = respect)
+  table <- facet_panels(facet, table, panel, coord, theme, geom_grobs)
+  table <- facet_axes(facet, table, panel, coord, theme)
+
+  # maxz returns zero if no guides is drawn
+  zero <- unit(0, "mm")
+  maxz <- function(x = list(), fun) if (length(x) == 0) zero else do.call("max", llply(x, fun))
+  
+  table$widths <- unit.c(maxz(table$grob[table$layout$l == 1], grobWidth),
+                         unit(1, "null"),
+                         maxz(table$grob[table$layout$l == 3], grobWidth))
+  table$heights <- unit.c(maxz(table$grob[table$layout$t == 1], grobHeight),
+                          unit(aspect_ratio, "null"),
+                          maxz(table$grob[table$layout$t == 3], grobHeight))
+  table
+}
+
+facet_panels.null <- function(facet, table, panel, coord, theme, geom_grobs) {
+  fg <- coord_render_fg(coord, panel$ranges[[1]], theme)
+  bg <- coord_render_bg(coord, panel$ranges[[1]], theme)
 
   # Flatten layers - we know there's only one panel
   geom_grobs <- lapply(geom_grobs, "[[", 1)
   panel_grobs <- c(list(bg), geom_grobs, list(fg))
   
-  panel_grob <- gTree(children = do.call("gList", panel_grobs))  
-
-  # generate grobs of positional guide
-  guides <- pguides_gengrob(c(panel$x_scales[1], panel$y_scales[1]), range, coord, theme)
-
-  # calculate placement of each guide
-  guides <- llply(guides,
-                  function(guide) {
-                    attr(guide, "place") <- switch(attr(guide, "position"),
-                                                top = c(2, 1), bottom = c(2, 3), left = c(1, 2), right = c(3, 2), float = c(2, 2))
-                    guide
-                  })
-
-  # maxz returns zero if no guides is drawn
-  zero <- unit(0, "null")
-  maxz <- function(x = list()) if (length(x) == 0) zero else do.call("max", x)
-
-  # calculate widths/heights by guides size
-  topHeight    <- maxz(llply(Filter(function(guide)attr(guide, "place")[2] == 1, guides), grobHeight))
-  bottomHeight <- maxz(llply(Filter(function(guide)attr(guide, "place")[2] == 3, guides), grobHeight))
-  leftWidth    <- maxz(llply(Filter(function(guide)attr(guide, "place")[1] == 1, guides), grobWidth))
-  rightWidth   <- maxz(llply(Filter(function(guide)attr(guide, "place")[1] == 3, guides), grobWidth))
-
-  # w/h of gtable
-  widths <- unit.c(leftWidth, unit(1, "null"), rightWidth)
-  heights <- unit.c(topHeight, unit(1, "null"), bottomHeight)
-
-  # create gtable
-  gt <- gtable(widths = widths, heights = heights, respect = respect)
-  
-  # panel
-  gt <- gtable_add_grob(gt, list(panel_grob), 2, 2, 2, 2, clip = "on", name = "panel")
-  for (guide in guides) {
-    gt <- gtable_add_grob(gt, list(guide), attr(guide, "place")[2], attr(guide, "place")[1], clip = "off", name = guide$name)
-  }
-  
-  gt
+  panel_grob <- gTree(children = do.call("gList", panel_grobs))
+  gtable_add_grob(table, list(panel_grob), 2, 2, clip = "on", name = paste("panel", panel_grob$name, sep = ""))
 }
-  
+
+facet_axes.null <- function(facet, table, panel, coord, theme) {
+  guides <- pguides_gengrob(c(panel$x_scales[1], panel$y_scales[1]), panel$ranges[[1]], coord, theme)
+  for (guide in guides) {
+    pos <- switch(attr(guide, "position"), top = c(1, 2), bottom = c(3, 2), left = c(2, 1), right = c(2, 3), float = c(2, 2))
+    table <- gtable_add_grob(table, list(guide), pos[1], pos[2], clip = "off", name = guide$name)
+  }
+  table
+}
+
 icon.facet_null <- function(.) {
   gTree(children = gList(
     rectGrob(0, 1, width=0.95, height=0.05, hjust=0, vjust=1, gp=gpar(fill="grey60", col=NA)),
